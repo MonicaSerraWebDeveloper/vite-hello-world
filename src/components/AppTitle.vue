875493139c5ae8f02223ca3960a62e7a57e8ed0d
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-10 14:44:18 +0200

    feat: add the title in App.vue with a components named AppTitle.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                firstTitleInVite: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ firstTitleInVite }}</h1>
+</template>
+
+<style>
+</style>
